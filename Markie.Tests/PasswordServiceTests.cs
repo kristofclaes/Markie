f543d770f73704886e6e13937ad792a5a82150dd
Kristof Claes <kristof.c@gmail.com>
2012-09-21 17:52:08 +0200

    Added tests for PasswordService

---
+++ Markie.Tests/PasswordServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using Markie.Authentication;
+using NUnit.Framework;
+
+namespace Markie.Tests
+{
+    [TestFixture]
+    public class PasswordServiceTests
+    {
+        private const string Salt = "salt";
+        private const string Password = "test";
+
+        [Test]
+        public void VerifyHashedPassword_returns_true_for_correct_password_and_correct_salt()
+        {
+            var passwordService = new PasswordService();
+
+            var hashedPassword = passwordService.HashPassword(Password, Salt);
+
+            Assert.IsTrue(passwordService.VerifyHashedPassword(Password, Salt, hashedPassword));
+        }
+
+        [Test]
+        public void VerifyHashedPassword_returns_false_for_incorrect_password_and_correct_salt()
+        {
+            var passwordService = new PasswordService();
+
+            var hashedPassword = passwordService.HashPassword(Password, Salt);
+
+            Assert.IsFalse(passwordService.VerifyHashedPassword("nottest", Salt, hashedPassword));
+        }
+
+        [Test]
+        public void VerifyHashedPassword_returns_false_for_correct_password_and_incorrect_salt()
+        {
+            var passwordService = new PasswordService();
+
+            var hashedPassword = passwordService.HashPassword(Password, Salt);
+
+            Assert.IsFalse(passwordService.VerifyHashedPassword(Password, "notsalt", hashedPassword));
+        }
+
+        [Test]
+        public void VerifyHashedPassword_returns_false_for_incorrect_password_and_incorrect_salt()
+        {
+            var passwordService = new PasswordService();
+
+            var hashedPassword = passwordService.HashPassword(Password, Salt);
+
+            Assert.IsFalse(passwordService.VerifyHashedPassword("nottest", "notsalt", hashedPassword));
+        }
+    }
+}
